6c207f1fb062ab3babdf65e8b526dcffd103eb91
vitalii <dumavb777@gmail.com>
2019-05-26 23:02:35 +0300

    sol

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const field = document.getElementById('field');
 const ball = document.getElementById('ball');
 const maxMoveInParentWidth = parseInt(getComputedStyle(field).width, 10);
 const maxMoveInParentHeight = parseInt(getComputedStyle(field).height, 10);
+const borderField = parseInt(getComputedStyle(field).borderWidth, 10);
+const marginBody = parseInt(getComputedStyle(document.body).margin, 10);
 const ballWith = parseInt(getComputedStyle(ball).width, 10);
 const ballHeight = parseInt(getComputedStyle(ball).height, 10);
 
@@ -9,8 +11,6 @@ function moveBall(event) {
     const { documentElement } = document;
     const coordinatesY = event.clientY + documentElement.scrollTop;
     const coordinatesX = event.clientX + documentElement.scrollLeft;
-    const borderField = parseInt(getComputedStyle(field).borderWidth, 10);
-    const marginBody = parseInt(getComputedStyle(document.body).margin, 10);
     const halfBallWidth = ballWith / 2;
     const halfBallHeight = ballHeight / 2;
     const lengthBorderMargin = borderField + marginBody;
